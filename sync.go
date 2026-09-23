0c0b6ab3aad87abcb7c3fd78f9ba199e14347355
sugvn <suganraj.1211@gmail.com>
2026-09-23 08:44:29 +0530

    feat: PullChanges
    add tests

--- sync.go
+++ sync.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	cp "github.com/otiai10/copy"
 )
 
 func Handle(err error){
@@ -52,8 +53,66 @@ func printDirMap(dir_map map[string]string){
 	}
 }
 
+func cleanUp(path string) error{
+	err:= os.RemoveAll(path)
+	if err!=nil {
+		fmt.Printf("cleanUp: Failed to remove directory: %s",path)
+		return err
+	}
+	return nil
+}
+
+func revertFromTmp(path string,dir_map map[string]string) error {
+	for basename := range dir_map {		tmpdir_basename:= path + "/" + basename
+		err:= os.Rename(tmpdir_basename,basename)
+		if err!=nil {
+			fmt.Println("Failed to move ",tmpdir_basename," to ",basename)
+			return err
+		}
+	}
+	return nil
+}
+
+// create a tmp directory in the current directory
+// for each item in the map,copy all files in the dotfiles/ into tmp directory
+// continue if succeeds,else abort
+// mv the files from the original location into the dotfiles directory
+// continue if succeeds,else copy the files from tmp into dotfiles
+// delete tmp
 func PullChanges(dir_map map[string]string) error {
+
 	// for key,val in dir_map: copy file(val) to file(key)	
+	tmpdir,err := os.MkdirTemp("./","dotsync-tmp")
+	if err!=nil {
+		return fmt.Errorf("Failed to create tmp directory: %w",err)
+	}
+	fmt.Println("Temp Directory Name:",tmpdir)
+
+	// mv all files into tmp
+	for basename := range dir_map {		
+		tmpdir_basename := tmpdir + "/" + basename
+		err := os.Rename(basename,tmpdir_basename)
+		if err!=nil {
+			fmt.Printf("failed to move %s into tmp: %s \n",basename,err.Error())
+		}
+	}
+
+	// copy all files from original path to root of dotfiles/
+	for basename,path := range dir_map {
+		err:= cp.Copy(path,basename)	
+		if err!=nil {
+			fmt.Printf("Failed to copy %s to %s: %s",path,basename,err.Error())
+			err1:= revertFromTmp(tmpdir,dir_map)
+			if err1!=nil {
+				fmt.Println("reverting from tmp failed: ",err1.Error())
+			} else {
+				fmt.Println("Cleaning up tmp")
+				cleanUp(tmpdir)
+			}
+			return err
+		}
+	}
+	cleanUp(tmpdir)
 	return nil
 }
 
--- sync_test.go
+++ sync_test.go
@@ -8,3 +8,11 @@ func TestBuildDirMap(t *testing.T){
 	Handle(err)
 	printDirMap(dir_map)
 }
+
+func TestPullChanges(t *testing.T){
+	filename:= "dirlist.txt"
+	dir_map,err:= BuildDirMap(filename)
+	Handle(err)
+	err1:= PullChanges(dir_map)
+	Handle(err1)
+}
