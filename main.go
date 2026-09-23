8b97a1076e2591159a2f8ae9ccb6c6553f73a817
sugvn <suganraj.1211@gmail.com>
2026-09-23 18:36:24 +0530

    add main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	dotsync "github.com/sugvn/dotsync/core"
+)
+
+func main() {
+	filename := "dirlist.txt"
+	dir_map,err := dotsync.BuildDirMap(filename)
+	if err != nil {
+		fmt.Println("Error building directory map:",err.Error())
+		return
+	}
+	// err = dotsync.PullChanges(dir_map)
+	// if err != nil {
+	// 	fmt.Println("Error Pulling Changes:")
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+}
