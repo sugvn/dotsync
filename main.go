00cfeee50f1794d1cba1c7c156da0a469d12b04a
sugvn <suganraj.1211@gmail.com>
2026-09-24 15:51:08 +0530

    feat: basic command line parsing

--- main.go
+++ main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"fmt"
 	dotsync "github.com/sugvn/dotsync/core"
+	"flag"
 )
 
 func main() {
-	filename := "dirlist.txt"
-	dir_map,err := dotsync.BuildDirMap(filename)
+	filename := flag.String("file","dirlist.txt","specify the file that lists the directories to track")
+	action := flag.String("action","none","specify whether to pull or push")
+	flag.Parse()
+	dir_map,err := dotsync.BuildDirMap(*filename)
 	if err != nil {
 		fmt.Println("Error building directory map:",err.Error())
 		return
 	}
-	// err = dotsync.PullChanges(dir_map)
-	// if err != nil {
-	// 	fmt.Println("Error Pulling Changes:")
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
+	if *action=="none" {
+		fmt.Println("No operation provided")
+		return
+	}
+	if *action=="pull" {
+		err = dotsync.PullChanges(dir_map)
+		if err != nil {
+			fmt.Println("Error Pulling Changes:")
+			fmt.Println(err.Error())
+			return
+		}
+	} else if *action=="push" {
+		fmt.Println("Unimplemented")
+	} else {
+		fmt.Println("no action named ",*action)
+	}
 }
